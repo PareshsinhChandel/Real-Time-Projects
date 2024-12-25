5dde74cf272762b0003279a3b905c75d62f6287a
pareshsinh chandel <pareshsinhchandel@gmail.com>
2024-12-25 13:54:20 +0530

    commit all the changes

--- To Do List/script.js
+++ To Do List/script.js
@@ -13,19 +13,38 @@ function AddTask() {
         let span = document.createElement('span');
         span.innerHTML = "\u00d7";
         li.appendChild(span);
-        saveData(); 
+
+        let editSpan = document.createElement('span');
+        editSpan.innerHTML = "\u270E"; // Pencil icon for editing
+        li.appendChild(editSpan);
+
+        saveData();
     }
     inputBox.value = '';
 
-    // Event listener for toggling task completion and removing task
+    // Event listener for toggling task completion and removing/editing task
     listContainer.addEventListener("click", function (e) {
         if (e.target.tagName === 'LI') {
             e.target.classList.toggle("checked");
-        } else if (e.target.tagName === "SPAN") {
-            e.target.parentNode.remove(); 
-            saveData(); // Save data to localStorage after removing task
+        } else if (e.target.innerHTML === "\u00d7") {
+            e.target.parentNode.classList.add('fade-out');
+            e.target.parentNode.addEventListener('animationend', function() {
+                e.target.parentNode.remove();
+                saveData();
+            });
+        } else if (e.target.innerHTML === "\u270E") {
+            editTask(e.target.parentNode);
         }
     }, false);
+}
+
+// Function to edit task
+function editTask(taskItem) {
+    let newTask = prompt("Edit your task", taskItem.firstChild.textContent);
+    if (newTask !== null && newTask !== "") {
+        taskItem.firstChild.textContent = newTask;
+        saveData();
+    }
 }
 
 // here we are storing data at localstorage
